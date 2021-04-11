4d5862f8d300c7777c06eda21eb32b4394427a04
Sungjoon Youn <ysjune1051@gmail.com>
2021-04-11 12:16:56 +0900

    20.category user 테이블 테스트

--- src/test/java/com/example/fcjava/repository/UserRepositoryTest.java
+++ src/test/java/com/example/fcjava/repository/UserRepositoryTest.java
@@ -22,17 +22,29 @@ public class UserRepositoryTest extends FcJavaApplicationTests {
 
     @Test
     public void create() {
-        // String sql = insert into user(%s, $s, %d) value (account, email, age);
+
+        String account = "Test01";
+        String password = "Test01";
+        String status = "REGISTERED";
+        String email = "Test01@gamil.com";
+        String phoneNumber = "010-1111-2222";
+        LocalDateTime registeredAt = LocalDateTime.now();
+        LocalDateTime createdAt = LocalDateTime.now();
+        String createdBy = "AdminServer";
+
         User user = new User();
-//        user.setId(1L); // auto increment
-        user.setAccount("TestUser03");
-        user.setEmail("TestUser03@gmail.com");
-        user.setPhoneNumber("010-1111-3333");
-        user.setCreatedAt(LocalDateTime.now());
-        user.setCreatedBy("TestUser3");
+        user.setAccount(account);
+        user.setPassword(password);
+        user.setStatus(status);
+        user.setEmail(email);
+        user.setPhoneNumber(phoneNumber);
+        user.setRegisteredAt(registeredAt);
+        user.setCreatedAt(createdAt);
+        user.setCreatedBy(createdBy);
 
         User newUser = userRepository.save(user);
-        System.out.println("newUser : " + newUser);
+
+        Assertions.assertNotNull(newUser);
 
     }
 
@@ -40,14 +52,10 @@ public class UserRepositoryTest extends FcJavaApplicationTests {
     @Transactional
     public void read() {
 
-        Optional<User> user = userRepository.findByAccount("TestUser03");
+        User user = userRepository.findFirstByPhoneNumberOrderByIdDesc("010-1111-2221");
 
-        user.ifPresent(selectUser -> {
-            selectUser.getOrderDetailList().stream().forEach(detail -> {
-                Item item = detail.getItem();
-                System.out.println(item);
-            });
-        });
+        Assertions.assertNotNull(user);
+
     }
 
     @Test

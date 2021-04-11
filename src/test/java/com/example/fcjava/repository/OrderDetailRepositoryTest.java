3c3972fa0ef2d471ccc74df33d6de08116f37dab
Sungjoon Youn <ysjune1051@gmail.com>
2021-04-11 14:37:57 +0900

    22.partner item order detail 테이블 테스트 - 2

--- src/test/java/com/example/fcjava/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/example/fcjava/repository/OrderDetailRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public class OrderDetailRepositoryTest extends FcJavaApplicationTests {
@@ -18,9 +19,14 @@ public class OrderDetailRepositoryTest extends FcJavaApplicationTests {
 
         OrderDetail orderDetail = new OrderDetail();
 
-        orderDetail.setOrderAt(LocalDateTime.now());
-//        orderDetail.setUserId(7L);
-//        orderDetail.setItemId(1L);
+        orderDetail.setStatus("WAITING");
+        orderDetail.setArrivalDate(LocalDateTime.now().plusDays(2));
+        orderDetail.setQuantity(1);
+        orderDetail.setTotalPrice(BigDecimal.valueOf(900000));
+        orderDetail.setOrderGroupId(1L);
+        orderDetail.setItemId(1L);
+        orderDetail.setCreatedAt(LocalDateTime.now());
+        orderDetail.setCreatedBy("AdminServer");
 
         OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
         Assertions.assertNotNull(newOrderDetail);
